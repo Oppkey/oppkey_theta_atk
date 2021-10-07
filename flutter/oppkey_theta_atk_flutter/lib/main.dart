83dd44401972a489c52bbcd076bb0b571635c853
Craig <craig@oppkey.com>
2021-10-07 15:02:31 -0700

    separated windows into separate files

--- flutter/oppkey_theta_atk_flutter/lib/main.dart
+++ flutter/oppkey_theta_atk_flutter/lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:oppkey_theta_atk_flutter/models/response_notifier.dart';
 import 'package:oppkey_theta_atk_flutter/models/video_notifier.dart';
+import 'package:oppkey_theta_atk_flutter/view/main_buttons.dart';
+import 'package:oppkey_theta_atk_flutter/view/main_window.dart';
 import 'package:theta/theta.dart';
 import 'package:thetaf/thetaf.dart';
 import 'package:provider/provider.dart';
@@ -27,55 +29,10 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Column(
+          // ignore: prefer_const_literals_to_create_immutables
           children: [
-            Expanded(
-                flex: 8,
-                child: context.watch<VideoNotifier>().videoRunning
-                    ? LivePreview(context.watch<VideoNotifier>().controller)
-                    : Text(context.watch<ResponseNotifier>().responseText)),
-            Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    OutlinedButton(
-                      onPressed: () async {
-                        var response = await ThetaBase.get('info');
-                        context
-                            .read<ResponseNotifier>()
-                            .setResponseText(response);
-                        print(response);
-                      },
-                      child: const Text('info'),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {
-                        context.read<VideoNotifier>().setVideoRunning(true);
-                        try {
-                          context.read<VideoNotifier>().controller.close;
-                        } catch (error) {
-                          print('controller already closed');
-                          print(error);
-                        }
-                        context
-                            .read<VideoNotifier>()
-                            .setController(StreamController());
-                        Preview.getLivePreview(
-                            frames: -1,
-                            controller: Provider.of<VideoNotifier>(context,
-                                    listen: false)
-                                .controller);
-                      },
-                      child: const Text('stream'),
-                    ),
-                    OutlinedButton(
-                        onPressed: () {
-                          context.read<VideoNotifier>().setVideoRunning(false);
-                          Preview.stopPreview();
-                        },
-                        child: const Text('stop video'))
-                  ],
-                )),
+            const MainWindow(),
+            const MainButtons(),
           ],
         ),
       ),
