6d377b72f86b8efd58a871b111b67f5b69562e5d
Craig <craig@oppkey.com>
2021-10-11 19:52:50 -0700

    fixed sc2 thumb problem

--- flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/sc2_thumb_get_bytes.dart
+++ flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/sc2_thumb_get_bytes.dart
@@ -24,6 +24,36 @@ Future<List<String>> sc2ThumbGetBytes({int number = 5}) async {
     for (var entry in fileList) {
       fileUrlList.add(entry['fileUrl']);
     }
+
+    if (number > 100) {
+      print('handling SC2 thumbs greater than 100');
+      int _totalEntries = responseMap['results']['totalEntries'];
+      if (number >= _totalEntries) {
+        number = _totalEntries;
+      }
+      int loops = (number / 100).truncate();
+      print('loops: $loops');
+      for (int i = 1; i <= loops; i++) {
+        int currentEntryCount = 100;
+        if (number - 100 * i < 100) {
+          currentEntryCount = number - 100 * i;
+        }
+        var response = await command(
+          'listFiles',
+          parameters: {
+            'fileType': 'image',
+            'startPosition': 100 * i,
+            'entryCount': currentEntryCount,
+            'maxThumbSize': 0
+          },
+        );
+        Map<String, dynamic> responseMap = jsonDecode(response);
+        var fileList = responseMap['results']['entries'];
+        for (var entry in fileList) {
+          fileUrlList.add(entry['fileUrl']);
+        }
+      }
+    }
     for (String fileUrl in fileUrlList) {
       var url = Uri.parse('$fileUrl?type=thumb');
       var response = await client.get(url, headers: headers);
