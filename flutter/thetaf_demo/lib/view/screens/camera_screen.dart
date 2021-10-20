336764a90dd0c79f2ff608fc70ed791ab597ea5f
Jesse Casman <jcasman@oppkey.com>
2021-10-20 10:03:57 -0700

    added camera screen and 2 working buttons

---
+++ flutter/thetaf_demo/lib/view/screens/camera_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:thetaf/thetaf.dart';
+
+class CameraScreen extends StatelessWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ThetaWindow(
+            child: Column(
+      children: [
+        const ResponseWindow(flex: 5),
+        Padding(
+          padding: const EdgeInsets.all(60.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              InfoButton(),
+              ResetButton(),
+            ],
+          ),
+        ),
+      ],
+    )));
+  }
+}
