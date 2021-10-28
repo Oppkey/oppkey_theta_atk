3954b45fc37731e2f4b0dbe7679550eff62f6bff
craig <craig@oppkey.com>
2021-10-28 15:12:53 -0700

    updated camera screen layout

--- flutter/oppkey_theta_atk_flutter/lib/view/screens/camera_screen.dart
+++ flutter/oppkey_theta_atk_flutter/lib/view/screens/camera_screen.dart
@@ -11,82 +11,99 @@ class CameraScreen extends StatelessWidget {
         body: ThetaWindow(
             child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: ListView(
+          child: Column(
             children: [
-              const ResponseWindow(flex: 5),
+              const ResponseWindow(flex: 4),
               // This row contains basic camera information commands
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  InfoButton(),
-                  SetModeImageButton(),
-                ],
-              ),
-              Row(
-                children: [
-                  SetModeVideoButton(),
-                  StartVideoCaptureButton(),
-                  StopVideoCaptureButton(),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              //This row is for controlling sleep settings.
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  DisableSleepDelayButton(),
-                  EnableSleepDelayButton(),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              //This rows is for controlling power off settings.
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  EnablePowerOffButton(),
-                  DisablePowerOffButton(),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              //This row contains commands that heavily change camera data or settings.
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  DeleteAllFilesButton(
-                    style: ButtonStyle(
-                      textStyle: MaterialStateProperty.all<TextStyle>(
-                        const TextStyle(fontSize: 42),
-                      ),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.red),
+              Expanded(
+                flex: 6,
+                child: ListView(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        SetModeImageButton(),
+                        SetModeVideoButton(),
+                      ],
                     ),
-                  ),
-                  const DeleteAllImagesButton(),
-                  const DeleteAllVideosButton(),
-                  const ResetButton(),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              //This row is for getting information on files in the camera.
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  List5FilesButton(),
-                  List5VideosButton(),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        StartVideoCaptureButton(),
+                        StopVideoCaptureButton(),
+                      ],
+                    ),
+
+                    //This row is for controlling sleep settings.
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        DisableSleepDelayButton(),
+                        EnableSleepDelayButton(),
+                      ],
+                    ),
+                    //This rows is for controlling power off settings.
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        EnablePowerOffButton(),
+                        DisablePowerOffButton(),
+                      ],
+                    ),
+
+                    //This row contains commands that heavily change camera data or settings.
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DeleteAllFilesButton(
+                          style: ButtonStyle(
+                            textStyle: MaterialStateProperty.all<TextStyle>(
+                              const TextStyle(fontSize: 16),
+                            ),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.red),
+                          ),
+                        ),
+                        ResetButton(
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.red),
+                          ),
+                        ),
+                        // const DeleteAllImagesButton(),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DeleteAllVideosButton(
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.red),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    //This row is for getting information on files in the camera.
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        List5FilesButton(),
+                        List5VideosButton(),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
