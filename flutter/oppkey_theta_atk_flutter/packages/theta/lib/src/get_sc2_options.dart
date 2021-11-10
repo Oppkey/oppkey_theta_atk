cc07a20331f46d2b3bce8ee50815da2ca2fe0b9a
craig <craig@oppkey.com>
2021-11-10 05:01:45 -0800

    moved to version 0.4.0

--- flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/get_sc2_options.dart
+++ flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/get_sc2_options.dart
@@ -1,5 +1,29 @@
 import '../theta.dart';
 
+/// Be aware that options not supported by SC2 will cause this program to crash.
+/// For a list of available APIs: https://api.ricoh/docs/theta-web-api-v2.1/
+/// For additional help or if you notice errors in the API documentation,
+/// please use https://www.theta360.guide
+/// 18 options can't be used with SC2
+/// * aperture
+/// * _authentication
+/// * _compositeShootingOutputInterval - Docs show not supported by SC2 or SC2B
+/// * _compositeShootingTime - Docs show not supported by SC2 or SC2B
+/// * _HDMIreso - Docs show supported only by S and SC
+/// * _imageStitching - Docs show not supported by SC2 or SC2B
+/// * _language - Docs show not supported by SC2 or SC2B
+/// * _microphone - Docs show not supported by SC2 or SC2B
+/// * _microphoneChannel
+/// * _networkType
+/// * _password
+/// * _shootingMethod
+/// * _timeShift - Docs show supported by SC2B, not SC2 (currently testing with SC2)
+/// * _topBottomCorrection
+/// * _username
+/// * videoStitching
+/// * _visibilityReduction
+/// * _wlanFrequency
+
 Future<String> getSc2Options() async {
   var response = await command('getOptions', parameters: {
     'optionNames': [
--- flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/get_z1_options.dart
+++ flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/get_z1_options.dart
@@ -1,5 +1,18 @@
 import '../theta.dart';
 
+/// Be aware that options not supported by Z1 will cause this program to crash.
+/// For a list of available APIs: https://api.ricoh/docs/theta-web-api-v2.1/
+/// For additional help or if you notice errors in the API documentation,
+/// please use https://www.theta360.guide
+/// 6 can't be used with Z1
+///
+/// * _bluetoothClassicEnable
+/// * _HDMIreso - only S and SC
+/// * _password
+/// * _shootingMethod - Can be acquired and set only when in the Still image shooting mode and _function is the My Settings shooting function.
+/// * _username
+/// * _wlanChannel
+
 Future<String> getZ1Options() async {
   var response = await command('getOptions', parameters: {
     'optionNames': [
