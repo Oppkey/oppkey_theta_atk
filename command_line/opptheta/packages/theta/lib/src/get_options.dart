8b2cd2a9eed105407678f245a7f0ff55176a5538
craig <craig@oppkey.com>
2021-11-13 17:26:28 -0800

    added additional documentation for getOptions

--- command_line/opptheta/packages/theta/lib/src/get_options.dart
+++ command_line/opptheta/packages/theta/lib/src/get_options.dart
@@ -1,5 +1,20 @@
 import '../theta.dart';
 
+/// Get camera options. The options must be a String list.
+///
+/// Example with inline options:
+///
+/// ```dart
+/// getOptions(['_filter', 'offDelay', 'sleepDelay']);
+/// ```
+///
+/// Example with variable
+///
+/// ```dart
+/// List<String> options = ['_filter', 'offDelay', 'sleepDelay'];
+/// getOptions(options);
+/// ```
+///
 Future<String> getOptions(List<String> options) async {
   var response =
       await command('getOptions', parameters: {'optionNames': options});
