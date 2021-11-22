b62270c44afc9bc1b0d9d1cd1793d09933f66472
craig <craig@oppkey.com>
2021-11-22 14:14:51 -0800

    corrected error in theta library

--- flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/set_option.dart
+++ flutter/oppkey_theta_atk_flutter/packages/theta/lib/src/set_option.dart
@@ -1,8 +1,7 @@
 import 'package:theta/src/command.dart';
 
 /// set single option
-Future<String> setOption(String s,
-    {required String name, required dynamic value}) async {
+Future<String> setOption({required String name, required dynamic value}) async {
   var response = await command(
     'setOptions',
     parameters: {
--- flutter/oppkey_theta_atk_flutter/packages/thetaf/lib/src/view/request/set_aperture_0_button.dart
+++ flutter/oppkey_theta_atk_flutter/packages/thetaf/lib/src/view/request/set_aperture_0_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:theta/theta.dart';
+import 'package:theta/theta.dart' as theta;
 import 'package:thetaf/src/model/response_notifier.dart';
 
 /// {@category Z1}
@@ -30,7 +30,7 @@ class SetAperture0Button extends StatelessWidget {
         autofocus: autofocus,
         clipBehavior: clipBehavior,
         onPressed: () async {
-          var response = await setOption(name: 'aperture', value: 0);
+          var response = await theta.setOption(name: 'aperture', value: 0);
           // set title for response. Edit the line below
           // for each option
           response = 'attempting to set option for aperture\n'
@@ -40,7 +40,7 @@ class SetAperture0Button extends StatelessWidget {
           // delay 250ms before sending the camera another command
           await Future.delayed(const Duration(milliseconds: 250));
           // send command to check the option you just set
-          var responseCheck = await command('getOptions', parameters: {
+          var responseCheck = await theta.command('getOptions', parameters: {
             'optionNames': ['aperture']
           });
           // combine the two strings
